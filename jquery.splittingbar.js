de8e8451d3010fe0a3340f21751b6c90cc161167
juanonsoftware <juanonsoftware@github.com>
2017-04-25 18:27:02 +0700

    If both numbers are 0, the bar should be splitting into two equal parts

--- jquery.splittingbar.js
+++ jquery.splittingbar.js
@@ -11,34 +11,36 @@
 </div> 
 */
 
-(function($){
-	
-  var defaults = {
-  	rightPartBgColor: '#4CAF50',
-    leftPartBgColor: '#DDD',
-    lineHeight: '20px',
-    leftNumber: 10,
-    rightNumber: 10
-  };
-  
-  function SplittingBar($element, options){
-  	this.$element = $element;
-    this.options = $.extend({}, defaults, options);
-    this.init();
-  }
-  
-  SplittingBar.prototype = {
-  	init: function(){
-    	var leftPercent = this.options.leftNumber / (this.options.leftNumber + this.options.rightNumber) * 100;
-    
-    	this.$element.html('<div style="width: 100%; background-color: ' + this.options.leftPartBgColor + '"><div style="width: '+ leftPercent +'%; background-color: ' + this.options.rightPartBgColor + '; text-align: center; line-height: ' + this.options.lineHeight + ';">&nbsp;</div></div>');
+(function ($) {
+
+    var defaults = {
+        rightPartBgColor: '#4CAF50',
+        leftPartBgColor: '#DDD',
+        lineHeight: '20px',
+        leftNumber: 10,
+        rightNumber: 10
+    };
+
+    function SplittingBar($element, options) {
+        this.$element = $element;
+        this.options = $.extend({}, defaults, options);
+        this.init();
     }
-  };
-  
-  $.fn.splittingBar = function(options){
-  	return $(this).each(function(){
-    	new SplittingBar($(this), options);
-    });
-  };
-  
+
+    SplittingBar.prototype = {
+        init: function () {
+            var leftPercent = this.options.leftNumber / (this.options.leftNumber + this.options.rightNumber) * 100;
+            if (this.options.leftNumber === 0 && this.options.rightNumber === 0) {
+                leftPercent = 50;
+            }
+            this.$element.html('<div style="width: 100%; background-color: ' + this.options.leftPartBgColor + '"><div style="width: ' + leftPercent + '%; background-color: ' + this.options.rightPartBgColor + '; text-align: center; line-height: ' + this.options.lineHeight + ';">&nbsp;</div></div>');
+        }
+    };
+
+    $.fn.splittingBar = function (options) {
+        return $(this).each(function () {
+            new SplittingBar($(this), options);
+        });
+    };
+
 })(jQuery);
